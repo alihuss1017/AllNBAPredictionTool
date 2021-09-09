0bd33d72cbecec9089f3d2e53a952862aced6c0d
alihuss1017 <83521645+alihuss1017@users.noreply.github.com>
2021-09-08 18:17:31 -0700

    Update LogRegModel.py

--- AllNBAPred/LogRegModel.py
+++ AllNBAPred/LogRegModel.py
@@ -13,12 +13,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
-pd.set_option('display.max_rows', None)
 
+best=0
 trueNegative = 0
 truePositive = 0
 falseNegative = 0
@@ -36,13 +32,21 @@ statList=[[],
 predList=[]
 actList=[]
 
+dictNba = {'Prediction': [],'PTS':[],'TRB':[],'AST':[],'GPnSround%':[], 'PER':[],'WS':[],'BPM':[],'VORP':[], 'Actual':[]}
+stats = 'PTS', 'TRB', 'AST', 'GPnSround%', 'PER', 'WS', 'BPM', 'VORP'
+predict = 'All-NBA?'
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.max_rows', None)
+
 dataSet = pd.read_csv("DataSet.csv")
 dataSet = dataSet[['Player','PTS', 'TRB', 'AST', 'GPnSround%', 'PER', 'WS', 'BPM', 'VORP', 'All-NBA?']]
 a=dataSet.pop('Player')
 A=np.array(a)
 
-dictNba = {'Prediction': [],'PTS':[],'TRB':[],'AST':[],'GPnSround%':[], 'PER':[],'WS':[],'BPM':[],'VORP':[], 'Actual':[]}
-predict = 'All-NBA?'
 X = np.array(dataSet.drop([predict], 1))
 trueX = X
 Y = np.array(dataSet[predict])
@@ -51,7 +55,7 @@ LogisticRegression(solver='lbfgs', max_iter=1000)
 min_max_scaler = preprocessing.MinMaxScaler()
 X = min_max_scaler.fit_transform(X)
 
-best=0
+
 for _ in range(50):
 
     trainTrueX, testTrueX, trainA, testA, trainX, testX, trainY, testY = sklearn.model_selection.train_test_split(trueX,A, X, Y, test_size=0.3)
@@ -68,7 +72,7 @@ pickle_in = open("studentmodel.pickle", "rb")
 logistic = pickle.load(pickle_in)
 logPredY= logistic.predict(testX)
 
-stats = 'PTS', 'TRB', 'AST', 'GPnSround%', 'PER', 'WS', 'BPM', 'VORP'
+
 for x in range(len(logPredY)):
 
     if logPredY[x]==0 and logPredY[x]==testY[x]:
@@ -98,56 +102,68 @@ dictNba['Actual']= actList
 
 Precision = float(precision_score(testY, logPredY))
 Recall = float(recall_score(testY, logPredY))
-f1Score = float((2*Precision*Recall)/(Precision+Recall))
-
-print(f'True Negatives: {trueNegative}')
-print(f'True Positives: {truePositive}')
-print(f'False Negatives: {falseNegative}')
-print(f'False Positives: {falsePositive}')
-
-print(f'Precision: {Precision}')
-print(f'Recall: {Recall}')
-print(f'f1Score: {f1Score}')
-
 dictNbaFrame = pd.DataFrame(dictNba)
 dictNbaFrame.index=playerList
-print(f'{dictNbaFrame}')
 
-plotStats = ['PTS','PER','WS','BPM','VORP']
-for stat in plotStats:
-    x1= stat
-    y1='GPnSround%'
+def printResults():
+    print(f'True Negatives: {trueNegative}')
+    print(f'True Positives: {truePositive}')
+    print(f'False Negatives: {falseNegative}')
+    print(f'False Positives: {falsePositive}')
+    print(f'Precision: {Precision}')
+    print(f'Recall: {Recall}')
+    print(f'{dictNbaFrame}')
 
-    ax = plt.axes(projection='3d')
-    x=dictNbaFrame[stat]
-    y=dictNbaFrame[y1]
-    z=dictNbaFrame['Prediction']
-    z2=dictNbaFrame['Actual']
+def statRelationPlot():
+    plotStats = ['PTS','PER','WS','BPM','VORP']
+    for stat in plotStats:
+        x1= stat
+        y1='GPnSround%'
 
-    ax.set_xlabel(f'{stat}')
-    ax.set_ylabel('% of Games Played and Started')
-    ax.set_zlabel(predict)
+        ax = plt.axes(projection='3d')
+        x=dictNbaFrame[stat]
+        y=dictNbaFrame[y1]
+        z=dictNbaFrame['Prediction']
+        z2=dictNbaFrame['Actual']
 
-    ax.scatter(x,y,z, label='Prediction')
-    ax.scatter(x,y,z2, label='Actual')
-    ax.set_title(f'Correlation of {stat} and Games Played with All-NBA Teams')
+        ax.set_xlabel(f'{stat}')
+        ax.set_ylabel('% of Games Played and Started')
+        ax.set_zlabel(predict)
+
+        ax.scatter(x,y,z, label='Prediction')
+        ax.scatter(x,y,z2, label='Actual')
+        ax.set_title(f'Correlation of {stat} and Games Played with All-NBA Teams')
+        plt.show()
+
+def tnfPlot():
+    tnfLabels = ['TP', 'FN', 'FP']
+    tnfResult = np.array([truePositive, falseNegative, falsePositive])
+
+    plt.bar(tnfLabels, tnfResult, width=0.3)
+    plt.yticks(np.arange(0, tnfResult.max()+1, 1))
+    plt.title('Results of Logistic Regression Algorithm')
     plt.show()
 
-tnfLabels = ['TP', 'FN', 'FP']
-tnfResult = np.array([truePositive, falseNegative, falsePositive])
+def pnrPlot():
+    pnrLabel = ['Precision', 'Recall']
+    pnrResult = np.array([Precision, Recall])
 
-plt.bar(tnfLabels, tnfResult, width=0.3)
-plt.yticks(np.arange(0, tnfResult.max()+1, 1))
-plt.title('Results of Logistic Regression Algorithm')
-plt.show()
+    plt.bar(pnrLabel, pnrResult, width = 0.5)
+    plt.yticks(np.arange(0,1.05,.05))
+    plt.title('Precision and Recall of Logistic Regression Algorithm')
+    plt.show()
 
-pnrLabel = ['Precision', 'Recall']
-pnrResult = np.array([Precision, Recall])
+def f1Score():
+    f1ScoreLR = float((2*Precision*Recall)/(Precision+Recall))
+    print(f'f1Score of Logistic Regression: {f1ScoreLR}')
+    return f1ScoreLR
 
-plt.bar(pnrLabel, pnrResult, width = 0.5)
-plt.yticks(np.arange(0,1.05,.05))
-plt.title('Precision and Recall of Logistic Regression Algorithm')
-plt.show()
+def main():
+    printResults()
+    statRelationPlot()
+    tnfPlot()
+    pnrPlot()
+    f1Score()
 
-
-
+if __name__ =="__main__":
+    main()
